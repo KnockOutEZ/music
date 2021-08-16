78af8bf8a6d55afc7ea9717b35539fd5a0c09713
tauhid <ktowhid@gmail.com>
2021-08-16 17:42:12 +0600

    rules in progress

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
@@ -25,6 +26,7 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const userCollection = db.collection('users');
 
@@ -32,4 +34,5 @@ export {
   auth,
   db,
   userCollection,
+  storage,
 };
